e541d092e5e29f92a9bae5870ed607863bd58948
Matt Walker <walkermatt@longwayaround.org.uk>
2014-03-19 19:51:51 +0000

    Better exception msg inc. invalid char and using protocols

--- clojure/rna-transcription/dna.clj
+++ clojure/rna-transcription/dna.clj
@@ -4,10 +4,19 @@
   "Look from DNA to RNA nucleotide."
   {\G \C, \C \G, \T \A, \A \U})
 
-(defn to-rna
-  "Convert a DNS strand to an RNA strand."
-  [dna]
-  (let [rna (apply str (map #(get m %) dna))]
-    (if (= (count dna) (count rna))
-      rna
-      (throw (AssertionError. (str "Invalid DNA strand. Only the nucleotides " (apply str (keys m)) " are valid."))))))
+(defn- ex-msg
+  [s]
+  (str "Invalid DNA strand. Only the nucleotides " (apply str (keys m)) " are valid. Found: " s))
+
+(defprotocol ToRna
+  "Returns the RNA complement of a given DNA nucleotide or strand"
+  (to-rna [dna]))
+
+(extend-protocol ToRna
+  java.lang.Character
+  (to-rna [dna]
+    (or (get m dna) (throw (AssertionError. (ex-msg dna)))))
+  java.lang.String
+  (to-rna [dna]
+    (apply str (map #(to-rna %) dna))))
+
