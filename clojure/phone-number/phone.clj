b8830155412a30a9e6c33d357a484452a972bc1f
Matt Walker <walkermatt@longwayaround.org.uk>
2014-03-25 19:54:03 +0000

    Solution to phone-number in Clojure

---
+++ clojure/phone-number/phone.clj
@@ -0,0 +1,24 @@
+(ns phone)
+
+(defn digits
+  [n]
+  (let [digits (re-seq #"\d" n)
+        cnt (count digits)]
+    (if (= cnt 10)
+      digits
+      (if (and (= cnt 11) (= (first digits) "1"))
+        (drop 1 digits)
+        (repeat 10 "0")))))
+
+(defn number
+  [n]
+  (apply str (digits n)))
+
+(defn area-code
+  [n]
+  (apply str (take 3 (digits n))))
+
+(defn pretty-print
+  [n]
+ (let [groups (re-find #"(\d{3})(\d{3})(\d{4})" (number n))]
+   (str "(" (nth groups 1) ") " (nth groups 2) "-" (nth groups 3))))
