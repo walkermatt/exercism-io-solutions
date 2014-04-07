b3ebd5cb7590acd2b6858c5b75e64cb0f9e38e79
Matt Walker <walkermatt@longwayaround.org.uk>
2014-04-07 21:54:23 +0100

    Update ETL to use zipmap on each entry to get a hash-map

--- clojure/etl/etl.clj
+++ clojure/etl/etl.clj
@@ -4,13 +4,13 @@
 (defn transform-entry
   "Transform an entry in the source map so that each value is a key with the
   key as a value:
-    [1 [APPLE ARTICHOKE]] => [apple 1 artichoke 1]"
+    [1 [APPLE ARTICHOKE]] => {apple 1 artichoke 1}"
   [[k v]]
-  (interleave (map st/lower-case v) (repeat k)))
+  (zipmap (map st/lower-case v) (repeat k)))
 
 (defn transform
   "Transform a map of scrabble scores keyed on score to a map keyed on
   lower-case letter or word:
-    {1 [APPLE ARTICHOKE] 2 [BOAT] } => {apple 1 artichoke 1 boat 2}"
+    {1 [APPLE ARTICHOKE] 2 [BOAT]} => {apple 1 artichoke 1 boat 2}"
   [m]
-  (apply hash-map (flatten (map transform-entry m))))
+  (into {} (map transform-entry m)))
