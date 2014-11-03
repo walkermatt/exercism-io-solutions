e553828618ed0a3bb779f3a6c3de0593212c8779
Matt Walker <walkermatt@longwayaround.org.uk>
2014-11-03 10:54:04 +0000

    Simplify based on previous submissions and nitpick from @toolkit

--- clojure/rna-transcription/dna.clj
+++ clojure/rna-transcription/dna.clj
@@ -1,22 +1,17 @@
 (ns dna)
 
 (def m
-  "Look from DNA to RNA nucleotide."
+  "Look-up from DNA to RNA nucleotide."
   {\G \C, \C \G, \T \A, \A \U})
 
 (defn- ex-msg
-  [s]
-  (str "Invalid DNA strand. Only the nucleotides " (apply str (keys m)) " are valid. Found: " s))
+  [dna]
+  (str "Invalid DNA strand. Only the nucleotides " (apply str (keys m)) " are valid. Found: " dna))
 
-(defprotocol ToRna
-  "Returns the RNA complement of a given DNA nucleotide or strand"
-  (to-rna [dna]))
-
-(extend-protocol ToRna
-  java.lang.Character
-  (to-rna [dna]
-    (or (get m dna) (throw (AssertionError. (ex-msg dna)))))
-  java.lang.String
-  (to-rna [dna]
-    (apply str (map #(to-rna %) dna))))
-
+(defn to-rna
+  "Convert a DNA strand to an RNA strand."
+  [dna]
+  (let [rna (apply str (map m dna))]
+    (if (= (count dna) (count rna))
+      rna
+      (throw (AssertionError. (ex-msg dna))))))
